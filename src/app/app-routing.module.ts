50a07f640f77a8bdb28417f2be98f10a2ca5f0e6
Adriano Melo <adrianoshout@gmail.com>
2021-11-22 17:14:51 -0300

    criação do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './telas/contato/contato.component';
+import { HomeComponent } from './telas/home/home.component';
+import { LoginComponent } from './telas/login/login.component';
+import { PoliticadeprivacidadeComponent } from './telas/politicadeprivacidade/politicadeprivacidade.component';
+import { ListarprodutosComponent } from './telas/produtos/listarprodutos/listarprodutos.component';
+import { RecuperarsenhaComponent } from './telas/recuperarsenha/recuperarsenha.component';
+import { TermosdeusoComponent } from './telas/termosdeuso/termosdeuso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "termosdeuso", component: TermosdeusoComponent},
+  { path: "politica", component: PoliticadeprivacidadeComponent},
+  { path: "", component: LoginComponent},
+  { path: "contato", component: ContatoComponent},
+  { path: "recuperarsenha", component: RecuperarsenhaComponent},
+  { path: "home", component: HomeComponent,
+      children: [
+        { path: "listarProdutos", component: ListarprodutosComponent},
+      ]
+  },
+  
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './telas/login/login.component';
+import { TermosdeusoComponent } from './telas/termosdeuso/termosdeuso.component';
+import { PoliticadeprivacidadeComponent } from './telas/politicadeprivacidade/politicadeprivacidade.component';
+import { HomeComponent } from './telas/home/home.component';
+import { ContatoComponent } from './telas/contato/contato.component';
+import { RecuperarsenhaComponent } from './telas/recuperarsenha/recuperarsenha.component';
+import { RodapeComponent } from './telas/rodape/rodape.component';
+import { NavComponent } from './telas/nav/nav.component';
+import { ListarprodutosComponent } from './telas/produtos/listarprodutos/listarprodutos.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TermosdeusoComponent,
+    PoliticadeprivacidadeComponent,
+    HomeComponent,
+    ContatoComponent,
+    RecuperarsenhaComponent,
+    RodapeComponent,
+    NavComponent,
+    ListarprodutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
